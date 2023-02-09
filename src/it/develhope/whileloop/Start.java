6dc9cbcaec8bcdbb0130d11d8c4a195141cb3616
Alessio Limina <alessio.limina@gmail.com>
2023-02-09 17:38:33 +0100

    Exercise While Loop 03

--- src/it/develhope/whileloop/Start.java
+++ src/it/develhope/whileloop/Start.java
@@ -1,20 +1,22 @@
 package it.develhope.whileloop;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class Start {
     public static void main(String[] args) {
 
         System.out.println("---------------Starting----------------------");
 
-        List<Integer> fibonacciNumbers = new ArrayList<>();
-        fibonacciNumbers.addAll(Arrays.asList(0, 1, 1, 2, 3, 5, 8, 13, 21, 34));
-        int i=0;
-        while (i<fibonacciNumbers.size()){
-            System.out.println(fibonacciNumbers.get(i));
-            i++;}
+        int i = 0;
+        int firstNumber = 0;
+        int secondNumber = 1;
+
+        while(i<10){
+            System.out.println(firstNumber);
+            int nextNumber = firstNumber + secondNumber;
+            firstNumber = secondNumber;
+            secondNumber = nextNumber;
+            i++;
+        }
 
         System.out.println("---------------------------------------------");
     }
