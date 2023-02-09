84b86b7f78eda724faa986c1c95f26b29e9f77a7
Alessio Limina <alessio.limina@gmail.com>
2023-02-09 18:01:58 +0100

    Exercise While Loop 03

--- src/it/develhope/whileloop/Start.java
+++ src/it/develhope/whileloop/Start.java
@@ -1,22 +1,41 @@
 package it.develhope.whileloop;
 
+/**
+ * This is a Tester Class
+ * @author alessiolimina
+ */
 
 public class Start {
     public static void main(String[] args) {
 
-        System.out.println("---------------Starting----------------------");
+        System.out.println("---------------Printing Fibonacci Numbers----------------------");
+
+        /**
+         * Declaring the variables
+         * @i is the value from we start to iterate
+         * @firstNumber is the first number
+         * @secondNumber is the secondNumber
+         */
 
         int i = 0;
         int firstNumber = 0;
         int secondNumber = 1;
+
+        /**
+         * Creating a While Loop
+         * Printing the firstNumber
+         * @nextNumber is a new variable, that is the sum between firstNumber and secondNumber
+         * Assigning to firstNumber the value of secondNumber
+         * Assigning to secondNumber the value of nextNumber
+         * Repeat until the end of loop
+         */
 
         while(i<10){
             System.out.println(firstNumber);
             int nextNumber = firstNumber + secondNumber;
             firstNumber = secondNumber;
             secondNumber = nextNumber;
-            i++;
-        }
+            i++;}
 
         System.out.println("---------------------------------------------");
     }
